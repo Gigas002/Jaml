7cacbe73a42f48ca2390569ceb9b885ce6b737ae
Gigas002 <gigas002@yandex.ru>
2020-12-14 18:26:15 +0300

    Add UIException class

---
+++ Jaml.Wpf/Exceptions/UIException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace Jaml.Wpf.Exceptions
+{
+    /// <inheritdoc />
+    public class UIException : Exception
+    {
+        /// <inheritdoc />
+        public UIException(string message) : base(message) { }
+
+        /// <inheritdoc />
+        public UIException(string message, Exception innerException) : base(message, innerException) { }
+
+        /// <inheritdoc />
+        public UIException() { }
+    }
+}
