b0f2c68a9210e8e9e58f179290ae3c6ee9d71e1a
Gigas002 <gigas002@yandex.ru>
2020-12-14 18:38:11 +0300

    Add CommandException

---
+++ Jaml.Wpf/Exceptions/CommandException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace Jaml.Wpf.Exceptions
+{
+    /// <inheritdoc />
+    public class CommandException : Exception
+    {
+        /// <inheritdoc />
+        public CommandException(string message) : base(message) { }
+
+        /// <inheritdoc />
+        public CommandException(string message, Exception innerException) : base(message, innerException) { }
+
+        /// <inheritdoc />
+        public CommandException() { }
+    }
+}
